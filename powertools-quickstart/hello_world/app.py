1f58714280f46ad7ac024102dc9e80a702dc68f0
gkuga <gaku.yasui@gmail.com>
2022-08-18 07:59:06 +0900

    Add handler of power tools

--- powertools-quickstart/hello_world/app.py
+++ powertools-quickstart/hello_world/app.py
@@ -1,38 +1,17 @@
-import json
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+
+app = APIGatewayRestResolver()
 
 
-def hello_name(event, **kargs):
-    username = event["pathParameters"]["name"]
-    return {"statusCode": 200, "body": json.dumps({"message": f"hello {username}!"})}
+@app.get("/hello/<name>")
+def hello_name(name):
+    return {"message": f"hello {name}!"}
 
 
-def hello(**kargs):
-    return {"statusCode": 200, "body": json.dumps({"message": "hello unknown!"})}
-
-
-class Router:
-    def __init__(self):
-        self.routes = {}
-
-    def set(self, path, method, handler):
-        self.routes[f"{path}-{method}"] = handler
-
-    def get(self, path, method):
-        try:
-            route = self.routes[f"{path}-{method}"]
-        except KeyError:
-            raise RuntimeError(
-                f"Cannot route request to the correct method. path={path}, method={method}")
-        return route
-
-
-router = Router()
-router.set(path="/hello", method="GET", handler=hello)
-router.set(path="/hello/{name}", method="GET", handler=hello_name)
+@app.get("/hello")
+def hello():
+    return {"message": "hello unknown!"}
 
 
 def lambda_handler(event, context):
-    path = event["resource"]
-    http_method = event["httpMethod"]
-    method = router.get(path=path, method=http_method)
-    return method(event=event)
+    return app.resolve(event, context)
