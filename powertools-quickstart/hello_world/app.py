f50d439cf2b17550a370ed832623d608a82abf96
gkuga <gaku.yasui@gmail.com>
2022-08-18 08:48:53 +0900

    Add aws x-lay

--- powertools-quickstart/hello_world/app.py
+++ powertools-quickstart/hello_world/app.py
@@ -1,6 +1,7 @@
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
+from aws_xray_sdk.core import xray_recorder
 
 logger = Logger(service="APP")
 
@@ -8,17 +9,20 @@ app = APIGatewayRestResolver()
 
 
 @app.get("/hello/<name>")
+@xray_recorder.capture('hello_name')
 def hello_name(name):
     logger.info(f"Request from {name} received")
     return {"message": f"hello {name}!"}
 
 
 @app.get("/hello")
+@xray_recorder.capture('hello')
 def hello():
     logger.info("Request from unknown received")
     return {"message": "hello unknown!"}
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True)
+@xray_recorder.capture('handler')
 def lambda_handler(event, context):
     return app.resolve(event, context)
