25bde0979c18e908d0b6a9fca31059f2b374d4cd
gkuga <gaku.yasui@gmail.com>
2022-08-18 08:52:46 +0900

    Improve trace with powertools

--- powertools-quickstart/hello_world/app.py
+++ powertools-quickstart/hello_world/app.py
@@ -1,46 +1,29 @@
-from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
-from aws_xray_sdk.core import patch_all, xray_recorder
 
 logger = Logger(service="APP")
-
+tracer = Tracer(service="APP")
 app = APIGatewayRestResolver()
-cold_start = True
-patch_all()
 
 
 @app.get("/hello/<name>")
-@xray_recorder.capture('hello_name')
+@tracer.capture_method
 def hello_name(name):
-    subsegment = xray_recorder.current_subsegment()
-    subsegment.put_annotation(key="User", value=name)
+    tracer.put_annotation(key="User", value=name)
     logger.info(f"Request from {name} received")
     return {"message": f"hello {name}!"}
 
 
 @app.get("/hello")
-@xray_recorder.capture('hello')
+@tracer.capture_method
 def hello():
-    subsegment = xray_recorder.current_subsegment()
-    subsegment.put_annotation(key="User", value="unknown")
+    tracer.put_annotation(key="User", value="unknown")
     logger.info("Request from unknown received")
     return {"message": "hello unknown!"}
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True)
-@xray_recorder.capture('handler')
+@tracer.capture_lambda_handler
 def lambda_handler(event, context):
-    global cold_start
-
-    subsegment = xray_recorder.current_subsegment()
-    if cold_start:
-        subsegment.put_annotation(key="ColdStart", value=cold_start)
-        cold_start = False
-    else:
-        subsegment.put_annotation(key="ColdStart", value=cold_start)
-
-    result = app.resolve(event, context)
-    subsegment.put_metadata("response", result)
-
-    return result
+    return app.resolve(event, context)
